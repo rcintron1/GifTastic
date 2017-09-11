884555ba2bf7c4f37bf090f1d853f00117e564c2
rcintron1 <rolando.cintron@gmail.com>
2017-09-10 23:43:39 -0400

    added a fancy jumbotron

---
+++ assets/javascript/app.js
@@ -0,0 +1,29 @@
+giphyKey = "bc8cb607d7f1414391f0c2f7c7432af8";
+
+$(document).ready(function() {
+  giphy = {};
+  giphy["g"]="https://media.giphy.com/media/l0Ex8HiCGD8BnsVvG/giphy.gif";
+  giphy["i"]="https://media.giphy.com/media/26gsjUq73y6M2VTSU/giphy.gif";
+  giphy["p"]="https://media.giphy.com/media/l0EwY7BdVn4C3ZajS/giphy.gif";
+  giphy["h"]="https://media.giphy.com/media/26gssxI5mUGopNIAg/giphy.gif";
+  giphy["y"]="https://media.giphy.com/media/l0ExaNkOKJ1UOW4ww/giphy.gif";
+  var txtGiphy;
+  giphyArray = "giphy".split("");
+  for(var i=0; i < giphyArray.length; i++ ){
+    var temp = $("<img>");
+    temp.attr('src', giphy[giphyArray[i]]);
+    temp.width(100);
+    temp.height(100);
+    $(".jumbotron").append(temp);
+    }
+
+    setInterval(function(){
+    num1 = Math.floor(Math.random() * (230 - 1) +1);
+    num2 = Math.floor(Math.random() * (230 - 1) +1);
+    num3 = Math.floor(Math.random() * (230 - 1) +1);
+    $(".jumbotron").animate({
+      backgroundColor: "rgb(" + num1+","+num2+","+num3+")",
+    }, 3000);
+  },3000);
+
+});
